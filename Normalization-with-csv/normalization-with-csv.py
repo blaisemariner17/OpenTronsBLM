937cd7e6eb115aa1063e8992c749649cc13204b6
blaisemariner17 <blaisemariner17@gmail.com>
2024-02-15 13:29:58 -0700

    fixing pipette misplacement command

--- Normalization-with-csv/normalization-with-csv.py
+++ Normalization-with-csv/normalization-with-csv.py
@@ -6,9 +6,9 @@ metadata = {
 }
 
 def run(ctx):
-    ###Using a csv to define the water and dna amounts: https://docs.opentrons.com/ot1/examples.html
-    #see input_csv.csv for formatting
 
+    #The first thing you MUST do before uploading this script into the OT-2 is to generate the dictionaries that tell the robot how much liquid to pipette where
+    #This is done by running the Generate_dictionaries_for_pooling-240208.jpy and copy/pasting the result here:
     ##############################################################
     ############### PASTE BELOW THESE HASHTAGS ###################
     ### (see normalization-with-csv-240208-blm.py for example) ###
@@ -20,25 +20,6 @@ def run(ctx):
     ##############################################################
     ##############################################################
     ##############################################################
-
-    with open('/home/blaise/blaisework-SMACK/PROJECTS/Active/3_opentrons/2024-02-07-writing-my-own-script/input_csv.csv') as my_file:
-            list_elements = my_file.read().splitlines()
-            if list_elements[0] == 'source_well,destination_well,water_volume,dna_volume':
-                for line in list_elements[1:len(list_elements)]:
-                    info = line.split(',')
-                    source_well = info[0]
-                    destination_well = info[1]
-                    water_vol = info[2]
-                    dna_vol = info[3]
-                    source_well_to_destination_well_dictionary[source_well] = destination_well
-                    water_addition_dictionary[destination_well] = water_vol
-                    DNA_addition_dictionary[source_well] = dna_vol 
-            else:
-                print('no')
-                raise ValueError("CSV NOT PROPERLY FORMATTED")
-    if len(source_well_to_destination_well_dictionary.keys()) == 0 or len(water_addition_dictionary.keys()) == 0 or len(DNA_addition_dictionary.keys()) == 0:
-        raise ValueError("VALUES MISSING FROM CSV PLEASE TRY AGAIN")
-
 
     #setting robot max speeds
     Default_max_speeds = {'X': 100, 'Y': 100, 'Z': 100, 'A': 100}
@@ -52,11 +33,11 @@ def run(ctx):
     water_container = ctx.load_labware('agilent_1_reservoir_290ml', 5).wells()[0]
     source_plate = ctx.load_labware("biorad_96_wellplate_200ul_pcr", 3)
     destination_plate = ctx.load_labware("biorad_96_wellplate_200ul_pcr", 6)
-    
+
     #the right pipette is 20ul and the left one is 200ul
     pipette_20 = ctx.load_instrument('p20_single_gen2', 'right', tip_racks=[tiprack_20_1, tiprack_20_2])
     pipette_200 = ctx.load_instrument('p300_single_gen2', 'left', tip_racks=[tiprack_200_1, tiprack_200_2])
-    
+
     #flow rates at ul/second
     pipette_20.flow_rate.aspirate = 2
     pipette_20.flow_rate.dispense = 2
@@ -64,7 +45,7 @@ def run(ctx):
     pipette_200.flow_rate.aspirate = 20
     pipette_200.flow_rate.dispense = 20
     pipette_200.flow_rate.blow_out = 20
-
+    
     #first, the water step-- we only need one tip for this so I will pick up the tip before the loop and then drop it after the loop
     # this loop gets water from the water_container and dispenses it into the destination_plate wells
     pipette_200.pick_up_tip()
